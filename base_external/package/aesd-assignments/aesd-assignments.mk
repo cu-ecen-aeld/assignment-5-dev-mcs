de23dca755d9de15388788ccbf90e949e282164f
mcs-dev <symes.m.c@gmail.com>
2022-10-12 20:03:58 +0100

    initial commit v1

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,11 +6,17 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '#COMMIT VERSION NUMBER'
+#AESD_ASSIGNMENTS_VERSION = '#COMMIT VERSION NUMBER'
+#AESD_ASSIGNMENTS_VERSION = '7defafd5c9cdb4f3985476f961125bc9eb88c4f4'
+#AESD_ASSIGNMENTS_VERSION = '325a825e6906d6db47a41557750c7f85e74c4957'
+#AESD_ASSIGNMENTS_VERSION = '7f3cc4400ec1f6bf0ff07ba55668b2096880e81c'
+AESD_ASSIGNMENTS_VERSION = '19cf2754eaa07236ac9eba4e8a7f65e2678410ac'
+
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
-AESD_ASSIGNMENTS_SITE = '#GITHUB REPOSITORY LINK'
+#AESD_ASSIGNMENTS_SITE = '#GITHUB REPOSITORY LINK'
+AESD_ASSIGNMENTS_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-dev-mcs.git'
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
@@ -23,6 +29,11 @@ define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -d 0755 $(@D)/conf/ $(TARGET_DIR)/etc/finder-app/conf/
 	$(INSTALL) -m 0755 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
 	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/bin
+
+	$(INSTALL) -m 0755 $(@D)/finder-app/writer $(TARGET_DIR)/bin
+	$(INSTALL) -m 0755 $(@D)/finder-app/finder.sh $(TARGET_DIR)/bin
+	$(INSTALL) -m 0755 $(@D)/finder-app/finder-test.sh $(TARGET_DIR)/bin
+
 endef
 
 $(eval $(generic-package))
