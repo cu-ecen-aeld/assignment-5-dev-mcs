8ef39a7fe5f09ae92dc549e01b30b304d2e0179e
mcs-dev <symes.m.c@gmail.com>
2022-10-22 22:41:32 +0100

    updates-v1

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,33 +6,23 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-#AESD_ASSIGNMENTS_VERSION = '#COMMIT VERSION NUMBER'
-#AESD_ASSIGNMENTS_VERSION = '7defafd5c9cdb4f3985476f961125bc9eb88c4f4'
-#AESD_ASSIGNMENTS_VERSION = '325a825e6906d6db47a41557750c7f85e74c4957'
-#AESD_ASSIGNMENTS_VERSION = '7f3cc4400ec1f6bf0ff07ba55668b2096880e81c'
-AESD_ASSIGNMENTS_VERSION = '19cf2754eaa07236ac9eba4e8a7f65e2678410ac'
+AESD_ASSIGNMENTS_VERSION = '648289b4c08c97c673fd3e5ae46882bb642a2730'
 
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
-#AESD_ASSIGNMENTS_SITE = '#GITHUB REPOSITORY LINK'
 AESD_ASSIGNMENTS_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-dev-mcs.git'
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
 define AESD_ASSIGNMENTS_BUILD_CMDS
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/server all
 endef
 
 # TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
-	$(INSTALL) -d 0755 $(@D)/conf/ $(TARGET_DIR)/etc/finder-app/conf/
-	$(INSTALL) -m 0755 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
-	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/bin
-
-	$(INSTALL) -m 0755 $(@D)/finder-app/writer $(TARGET_DIR)/bin
-	$(INSTALL) -m 0755 $(@D)/finder-app/finder.sh $(TARGET_DIR)/bin
-	$(INSTALL) -m 0755 $(@D)/finder-app/finder-test.sh $(TARGET_DIR)/bin
+	$(INSTALL) -m 0755 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/server/S99aesdsocket $(TARGET_DIR)/etc/init.d
 
 endef
 
